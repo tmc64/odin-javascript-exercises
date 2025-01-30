99f5c150d2087cff8da8c8bd914dc832bbea9b31
Josh Hyde <joshpaulhyde@gmail.com>
2025-01-30 16:29:45 +0000

    1-3 completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let repeatedString = ''
+    if (num < 0) {
+        return 'ERROR'
+        } else {
+            for (let i = 1; i <= num; i++) {
+                repeatedString += string
+            }
+        }
+    return repeatedString
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = ""
+    const stringArray = string.split("")
+    stringArray.reverse()
+    for (const i of stringArray) {
+        reversedString += i
+    }
+    return reversedString
 };
 
 // Do not edit below this line
