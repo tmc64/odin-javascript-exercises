90bec9c8058cadfbfae527f60b65f0ae6f875d49
Josh Hyde <joshpaulhyde@gmail.com>
2025-01-31 10:14:50 +0000

    4-6 completed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...args) {
+    let newArray = array.filter(function(a) {
+        return !args.includes(a)
+    }) 
+    return newArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR"
+    } else if (x < y) {
+        let sum = 0
+        for (let i = x; x <= y; x++) {
+            sum += x
+        }
+        return sum
+    } else {
+        let sum = 0
+        for (let i = x; x >= y; x--) {
+            sum += x
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
