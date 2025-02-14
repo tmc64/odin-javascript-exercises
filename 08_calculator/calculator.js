1587b3652fc7413549222401132ce069bb0fc4a6
Josh Hyde <joshpaulhyde@gmail.com>
2025-02-14 12:31:15 +0000

    8 completed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return sumValue = 0
+  } else {
+	return sumValue = array.reduce((sumValue, currentValue) => {
+    return sumValue + currentValue
+  })}
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) {
+    return multValue = 0
+  } else {
+	return multValue = array.reduce((multValue, currentValue) => {
+    return multValue * currentValue
+  })}
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1
+  for (let i = x; i > 0; i--) {
+    result = result * i
+  }
+	return result
 };
 
 // Do not edit below this line
