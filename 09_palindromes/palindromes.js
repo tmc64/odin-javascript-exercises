33b8d46a3b0227ad5a8c6f16ac414e9e56808eff
Josh Hyde <joshpaulhyde@gmail.com>
2025-02-14 13:37:48 +0000

    9 completed.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerString = string.toLowerCase()
+    const lowerArray = lowerString.split("")
+    let allowedCharacters = "1234567890qwertyuiopasdfghjklzxcvbnm"
+    const allowedArray = allowedCharacters.split("")
+    
+    const filteredArray = lowerArray.filter(function(validChars) {
+        return allowedArray.includes(validChars)
+    })
 
+    let originalString = ""
+    for (const i of filteredArray) {
+        originalString += i
+    }
+
+    filteredArray.reverse()
+
+    let reversedString = ""
+    for (const i of filteredArray) {
+        reversedString += i
+    }
+
+    return originalString === reversedString
 };
 
 // Do not edit below this line
