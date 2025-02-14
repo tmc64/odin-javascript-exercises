e9c7d71bb16a97d992055389ca62f09c3fa53360
Josh Hyde <joshpaulhyde@gmail.com>
2025-02-14 14:04:38 +0000

    10 completed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(x) {
+    if (x < 0) {
+        return "OOPS"
+    } else if (x == 0) {
+        return 0
+    } else {
+        let currentNumber = 1
+        let lastNumber = 0
+        for (i = 1; i < x; i++) {
+            let newNumber = currentNumber + lastNumber
+            lastNumber = currentNumber
+            currentNumber = newNumber        
+        }
+        return currentNumber
+    }
+}
 // Do not edit below this line
 module.exports = fibonacci;
